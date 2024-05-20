27b4b6f7640e1cf0f07e3c7009a8d262d8a41614
Muhammad Sarfaraz Tahir <159230707+muhammadsarfaraztahir@users.noreply.github.com>
2024-05-20 08:56:43 +0500

    Add files via upload

--- E commers website/index.js
+++ E commers website/index.js
@@ -1,1 +1,15 @@
-javascript :-)
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener("click", () => {
+        nav.classList.add('active');
+    })
+}
+if(bar){
+    close.addEventListener("click", () => {
+        nav.classList.remove('active');
+    })
+}
+
